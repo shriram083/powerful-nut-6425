4f5fd4ac813c40934d6ad8feb5cec2f99e565512
Shriram <shriramdeshpande77@gmail.com>
2022-08-25 22:35:37 +0530

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.png";
+import Integration from "./navbarComponents/Integration";
 
 const Navbar = () => {
   return (
-    <Flex justifyContent={"center"} gap={"30px"} padding={"30px"}>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/signup"}>Sign Up</Link>
-      <Link to={"/login"}>Login</Link>
-      <Link to={"/price"}>Price</Link>
-      <Link to={"/demo"}>Demo</Link>
+    <Flex
+      alignItems={"center"}
+      justifyContent={"space-between"}
+      gap={"30px"}
+      padding={"20px 30px"}
+      borderBottom="1px solid gray"
+    >
+      <Flex gap={"30px"} alignItems="center">
+        <Link to={"/"}>
+          <Image src={logo} />
+        </Link>
+        <Integration />
+        <Link to={"/price"}>Price</Link>
+        <Link to={"/demo"}>Demo</Link>
+      </Flex>
+      <Flex gap={"30px"} alignItems="center">
+        <Link to={"/login"}>Login</Link>
+        <Link to={"/signup"}>
+          <Box
+            backgroundColor={"#57bb71"}
+            color={"white"}
+            padding={"8px 15px"}
+            borderRadius={"6px"}
+          >
+            Sign Up
+          </Box>
+        </Link>
+      </Flex>{" "}
     </Flex>
   );
 };
