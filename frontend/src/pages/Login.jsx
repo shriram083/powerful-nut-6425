8ef2663e884d9d09223d8fc6233198b47d11832d
Shriram <shriramdeshpande77@gmail.com>
2022-08-27 22:31:50 +0530

    some changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { FaGithub } from 'react-icons/fa';
-import axios from "axios" 
+import { FaGithub } from "react-icons/fa";
+import axios from "axios";
 
 import "./css/login.css";
 import {
@@ -19,41 +19,37 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { loginreq } from "../reducer/authReducer/action";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-  const navigate=useNavigate()
-  const [email,setEmail]=useState("");
-  const [password,setPassword]=useState("");
-  const dispatch=useDispatch();
-const [url,setUrl]=useState("");
-useEffect(()=>{
-  axios.get("https://obscure-reef-85874.herokuapp.com/auth/github/req").then(res=>setUrl(res.data)).catch(err=>console.log("hello"));
-},[])
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const [url, setUrl] = useState("");
+  useEffect(() => {
+    axios
+      .get("https://obscure-reef-85874.herokuapp.com/auth/github/req")
+      .then((res) => setUrl(res.data))
+      .catch((err) => console.log("hello"));
+  }, []);
 
-const token=localStorage.getItem("token");
-if(token){
-
-  navigate("/");
-}
-
-const handleLogin=()=>{
-  const payload={
-    email:email,
-    password:password
-  }
-  dispatch(loginreq(payload));
+  const token = localStorage.getItem("token");
+  if (token) {
+    navigate("/");
   }
 
-
-
-
-
-
+  const handleLogin = () => {
+    const payload = {
+      email: email,
+      password: password,
+    };
+    dispatch(loginreq(payload));
+  };
 
   return (
-    <Box>
+    <Box mt={"150px"}>
       <Stack align={"center"}>
         <Heading fontSize={"4xl"}>Log in to Everhour</Heading>
         <Text fontSize={"lg"} color={"gray.600"}>
@@ -89,14 +85,16 @@ const handleLogin=()=>{
             <Stack spacing={6}>
               <Center p={"30px 20px 0px 20px"}>
                 <Button
-                // onClick={handleGit}
+                  // onClick={handleGit}
                   w={"full"}
                   maxW={"md"}
                   variant={"outline"}
                   leftIcon={<FaGithub />}
                 >
                   <Center>
-                    <Text><a href={url}>Login with github</a></Text>
+                    <Text>
+                      <a href={url}>Login with github</a>
+                    </Text>
                   </Center>
                 </Button>
               </Center>
@@ -105,32 +103,45 @@ const handleLogin=()=>{
               >
                 or
               </Text>
-          
-                <Input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" color={"black"} placeholder="ENTER YOUR EMAIL..." p={6} />
-            
-                <Input value={password} type="password" onChange={(e)=>setPassword(e.target.value)} placeholder="Password..."  p={6} />
-            
+
+              <Input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                color={"black"}
+                placeholder="ENTER YOUR EMAIL..."
+                p={6}
+              />
+
+              <Input
+                value={password}
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password..."
+                p={6}
+              />
+
               <Stack spacing={2}>
                 <Stack
                   direction={{ base: "column", sm: "row" }}
                   align={"start"}
                   justify={"space-between"}
                 ></Stack>
-               <Box margin={"auto"} >
-               <Button
-               onClick={handleLogin}
-                p={6}
-                w="70%"
-                margin={"auto"}
-                  bg={"green.400"}
-                  color={"white"}
-                  _hover={{
-                    bg: "green.500",
-                  }}
-                >
-                  Log in
-                </Button>
-               </Box>
+                <Box margin={"auto"}>
+                  <Button
+                    onClick={handleLogin}
+                    p={6}
+                    w="70%"
+                    margin={"auto"}
+                    bg={"green.400"}
+                    color={"white"}
+                    _hover={{
+                      bg: "green.500",
+                    }}
+                  >
+                    Log in
+                  </Button>
+                </Box>
                 <Link>Login with SSO</Link>
                 <Link>Reset password</Link>
               </Stack>
@@ -156,17 +167,29 @@ const handleLogin=()=>{
             </Text>
             <Heading textAlign={"left"} fontSize={"xl"}>
               Integration with Todoist
-              
             </Heading>
             <Text color="#848484" textAlign={"left"}>
-              Everhour now integrates with Todoist! 
-              <Link href="https://everhour.com/blog/everhour-now-integrates-with-todoist/" color={"blue"} > Learn more</Link>
+              Everhour now integrates with Todoist!
+              <Link
+                href="https://everhour.com/blog/everhour-now-integrates-with-todoist/"
+                color={"blue"}
+              >
+                {" "}
+                Learn more
+              </Link>
             </Text>
             <Heading textAlign={"left"} fontSize={"xl"}>
               Integration with Notion
             </Heading>
-            <Text color="#848484" textAlign={"left"}>Everhour now integrates with Notion!
-            <Link href="https://everhour.com/blog/everhour-now-integrates-with-todoist/" color={"blue"} > Learn more</Link>
+            <Text color="#848484" textAlign={"left"}>
+              Everhour now integrates with Notion!
+              <Link
+                href="https://everhour.com/blog/everhour-now-integrates-with-todoist/"
+                color={"blue"}
+              >
+                {" "}
+                Learn more
+              </Link>
             </Text>
             <Heading textAlign={"left"} fontSize={"xl"}>
               Single Sign-On
@@ -174,36 +197,43 @@ const handleLogin=()=>{
             <Text textAlign={"left"} color="#848484">
               Use single sign-on (SSO) to access Everhour for your projects and
               timesheets
-              <Link href="https://everhour.com/blog/everhour-now-integrates-with-todoist/" color={"blue"} > Learn more</Link>
+              <Link
+                href="https://everhour.com/blog/everhour-now-integrates-with-todoist/"
+                color={"blue"}
+              >
+                {" "}
+                Learn more
+              </Link>
             </Text>
           </Box>
         </Flex>
       </Box>
-      
-        <VStack spacing={8} w="50%" margin={"auto"} >
+
+      <VStack spacing={8} w="50%" margin={"auto"}>
         <Heading>New to Everhour?</Heading>
         <Text>Try Everhour for free and see if it works for your business</Text>
-        <Flex w="70%" margin={"auto"}  >
+        <Flex w="70%" margin={"auto"}>
           <Input marginRight={4} p={6} placeholder="Work email..."></Input>
           <Button p={6} bg={"green.400"} color="white">
             Try this free
           </Button>
         </Flex>
-        
-        </VStack>
-        <Center p={8}>
-          <Button
-            w={"full"}
-            maxW={"md"}
-            variant={"outline"}
-            leftIcon={<FaGithub />}
-          >
-            <Center>
-              <Text><a href={url}>Or sign up with Google Account</a></Text>
-            </Center>
-          </Button>
-        </Center>
-      </Box>
+      </VStack>
+      <Center p={8}>
+        <Button
+          w={"full"}
+          maxW={"md"}
+          variant={"outline"}
+          leftIcon={<FaGithub />}
+        >
+          <Center>
+            <Text>
+              <a href={url}>Or sign up with Google Account</a>
+            </Text>
+          </Center>
+        </Button>
+      </Center>
+    </Box>
   );
 };
 
